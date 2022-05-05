f9d3f7c858a0465d7116de8aabd90cd5dc78576b
Xuhandsome <chinaops666@gmail.com>
2022-05-06 02:16:35 +0800

    调整菜单栏样式

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,25 +1,34 @@
 <template>
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
         <h3>通用后台管理系统</h3>
-    <el-menu-item v-for="item in noChildren" :index="item.path" :key="item.path">
-        <i :class="'el-icon-' + item.icon"></i>
-        <span slot="title">{{item.label}}</span>
-    </el-menu-item>
-    <el-submenu v-for="item in hasChildren" :index="item.path" :key="item.path">
-        <template slot="title">
-        <i :class="'el-icon-' + item.icon"></i>
-            <span slot="title">{{ item.label }}</span>
-        </template>
-        <el-menu-item-group v-for="(subItem, subIndex) in item.children" :key="subItem.path">
-            <el-menu-item :index="subIndex">{{subItem.label}}</el-menu-item>
-        </el-menu-item-group>
-    </el-submenu>
+        <el-menu-item v-for="item in noChildren" :index="item.path" :key="item.path">
+            <i :class="'el-icon-' + item.icon"></i>
+            <span slot="title">{{item.label}}</span>
+        </el-menu-item>
+        <el-submenu v-for="item in hasChildren" :index="item.path" :key="item.path">
+            <template slot="title">
+            <i :class="'el-icon-' + item.icon"></i>
+                <span slot="title">{{ item.label }}</span>
+            </template>
+            <el-menu-item-group v-for="(subItem, subIndex) in item.children" :key="subItem.path">
+                <el-menu-item :index="subIndex">{{subItem.label}}</el-menu-item>
+            </el-menu-item-group>
+        </el-submenu>
     </el-menu>
 </template>
-<style>
+<style lang="less" scoped>
   .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 200px;
     min-height: 400px;
+  }
+  .el-menu {
+      height: 100%;
+      border: none;
+      h3 {
+          color: #fff;
+          text-align: center;
+          line-height: 48px
+      }
   }
 </style>
 
