ac178e373f7aa04d50bb145b149ae44052499b5e
Xuhandsome <chinaops666@gmail.com>
2022-05-06 00:39:30 +0800

    router使用

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
+import { Button,Radio } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import router from './view/router/Router.js' //引用Router.js声明为router
+import router from '../router/index.js'
 import VueRouter from 'vue-router'
-import axios from 'axios';
-import qs from 'qs'
+
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Radio)
 Vue.use(VueRouter)
-
-Vue.prototype.$axios = axios //全局注册，使用方法为: this.$axios
-axios.defaults.baseURL = '/api' //每次发送请求都会带一个/的前缀 跨域问题step1
-Vue.prototype.qs = qs //全局注册，使用方法为:this.qs
 
 new Vue({
   router: router, //将router放在Vue组件容器，进行全局生效
---
+++ views/User.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>我是User页面</div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'User',
+    data () {
+        return {}
+    }
+}
+</script>
