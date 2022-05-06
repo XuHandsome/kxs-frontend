04b530cb9a9e65dbb89832dd52c5829eacfae657
Xuhandsome <chinaops666@gmail.com>
2022-05-06 23:26:39 +0800

    解决重复点击路由报错的BUG

--- router/index.js
+++ router/index.js
@@ -3,6 +3,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+// 解决重复点击路由报错的BUG
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+
 const routes = [
     {
         path: '/',
