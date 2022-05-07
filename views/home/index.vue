98a1689a2df1ca4fabb7f54a4ae32bc579495a23
Xuhandsome <chinaops666@gmail.com>
2022-05-08 01:50:35 +0800

    echarts画折线图

--- views/home/index.vue
+++ views/home/index.vue
@@ -37,6 +37,7 @@
                 </el-card>
             </div>
             <el-card style="height: 280px">
+            <div style="height: 280px" ref="echarts"></div>
             </el-card>
             <div class="graph">
                 <el-card style="height: 260px"></el-card>
@@ -47,6 +48,7 @@
 </template>
 <script>
 import {getData} from '../../api/data.js'
+import * as echarts from 'echarts'
 
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
@@ -106,6 +108,31 @@ export default {
             const { code, data } = res.data
             if (code === 20000) {
                 this.tableData = data.tableData
+                const order = data.orderData
+                const xData = order.date
+                const keyArray = Object.keys(order.data[0])
+                const series = []
+                keyArray.forEach(key => {
+                    series.push({
+                        name: key,
+                        data: order.data.map(item => item[key]),
+                        type: 'line'
+                    })
+                })
+
+                const option = {
+                    xAxis: {
+                        data: xData
+                    },
+                    yAxis: {},
+                    legend: {
+                        data: keyArray
+                    },
+                    series
+                }
+
+                const E = echarts.init(this.$refs.echarts)
+                E.setOption(option)
             }
             console.log(res)
         })
