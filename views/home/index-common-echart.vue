f3616d8181b95fec33974dbfe45e5b3a729910d1
Xuhandsome <chinaops666@gmail.com>
2022-05-08 23:04:52 +0800

    抽出echart三种画图公用部分，减少代码量

---
+++ views/home/index-common-echart.vue
@@ -0,0 +1,168 @@
+<template>
+    <el-row class="home" :gutter="20">
+        <el-col :span="8" style="margin-top: 20px">
+            <el-card shadow="hover">
+                <div class="user">
+                    <img :src="userImg" />
+                    <div class="userinfo">
+                        <p class="name">Admin</p>
+                        <p class="access">超级管理员</p>
+                    </div>
+                </div>
+                <div class="login-info">
+                    <p>上次登陆时间: <span>2022-05-07</span></p>
+                    <p>上次登陆地点: <span>上海</span></p>
+                </div>
+            </el-card>
+            <el-card style="margin-top: 20px; height: 460px">
+                <el-table :data="tableData">
+                    <el-table-column
+                    v-for="(val, key) in tableLabel"
+                    :key="key"
+                    :prop="key"
+                    :label="val"
+                    >
+                    </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+        <el-col style="margin-top: 20px" :span="16">
+            <div class="num">
+                <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex', padding: 0 }">
+                    <i class="icon" :class="`el-icon-${item.icon}`" :style="{ background: item.color }"></i>
+                    <div class="detail">
+                        <p class="num">&yen;{{item.value}}</p>
+                        <p class="txt">{{item.name}}</p>
+                    </div>
+                </el-card>
+            </div>
+            <el-card style="height: 280px">
+                <echart :chartData="echartData.order" style="height: 280px" />
+            </el-card>
+            <div class="graph">
+                <el-card style="height: 260px">
+                    <echart :chartData="echartData.user" style="height: 240px" />
+                </el-card>
+                <el-card style="height: 260px">
+                    <echart :chartData="echartData.video" :isAxisChart="false" style="height: 240px" />
+                </el-card>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+<script>
+import {getData} from '../../api/data.js'
+import Echart from '../../src/components/CommonECharts.vue'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'home',
+    components: {
+        Echart
+    },
+    data () {
+        return {
+            userImg: require('../../src/assets/images/user.png'),
+            tableData: [],
+            tableLabel: {
+                name: '课程',
+                todayBuy: '今日购买',
+                monthBuy: '本月购买',
+                totalBuy: '总购买',
+            },
+            countData: [
+                {
+                    name: "今日支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "今日收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "今日未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+                {
+                    name: "本月支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "本月收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "本月未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+            ],
+            echartData: {
+                order: {
+                    xData: [],
+                    series: []
+                },
+                user: {
+                    xData: [],
+                    series: []
+                },
+                video: {
+                    series: []
+                }
+            }
+        }
+    },
+    mounted () {
+        getData().then(res => {
+            const { code, data } = res.data
+            if (code === 20000) {
+                this.tableData = data.tableData
+                const order = data.orderData
+                const xData = order.date
+                const keyArray = Object.keys(order.data[0])
+                const series = []
+                keyArray.forEach(key => {
+                    series.push({
+                        name: key,
+                        data: order.data.map(item => item[key]),
+                        type: 'line'
+                    })
+                })
+                this.echartData.order.xData = xData
+                this.echartData.order.series = series
+                this.echartData.user.xData = data.userData.map( item => item.date),
+                this.echartData.user.series = [
+                    {
+                        name: '新增用户',
+                        data: data.userData.map(item => item.new),
+                        type: 'bar', //柱状图
+                    },
+                    {
+                        name: '活跃用户',
+                        data: data.userData.map(item => item.active),
+                        type: 'bar', //柱状图
+                    }
+                ]
+                this.echartData.video.series = [
+                    {
+                        data: data.videoData,
+                        type: 'pie'
+                    }
+                ]
+            }
+            console.log(res)
+        })
+    }
+}
+</script>
